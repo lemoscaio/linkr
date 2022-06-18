2db2e5cc8d6fe943ae28dce377ff19994152e956
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-18 12:07:44 -0300

    <fix> : bug fixed button publish

--- src/pages/TimelinePage.js
+++ src/pages/TimelinePage.js
@@ -83,36 +83,32 @@ export default function TimelinePage() {
   return (
     <S.PageContainer>
       <PageLabel>timeline</PageLabel>
-      <S.PublishBox>
-        <img alt="" src="" />
-        <h2>What are you going to share today?</h2>
-        <form className="input-box" onSubmit={publishUrl}>
-          <input
-            className="input-url"
-            type="text"
-            disabled={activeButtonPublish}
-            placeholder="http://..."
-            value={publication.shared_url}
-            onChange={(e) =>
-              setPublication({ ...publication, shared_url: e.target.value })
-            }
-          />
+      <S.PublishCard>
+        <S.PostCardLeftColumn>
+            <S.CardProfileImage src={user.profileImage} alt={user.username} />
+        </S.PostCardLeftColumn>
+        <S.PostCardRightColumn>
+            <h2>What are you going to share today?</h2>
+            <form className="input-box" onSubmit={publishUrl} >
+              <input className="input-url"
+                  type="text"
+                  disabled={activeButtonPublish}
+                  placeholder="http://..." 
+                  value={publication.shared_url} 
+                  onChange={(e)=>setPublication({...publication, shared_url: e.target.value})}/>
 
-          <input
-            className="input-message"
-            type="text"
-            disabled={activeButtonPublish}
-            placeholder="What's on your mind?"
-            value={publication.message}
-            onChange={(e) =>
-              setPublication({ ...publication, message: e.target.value })
-            }
-          />
-          <button className="button-publish" type="submit">
-            {loadingPublish}
-          </button>
-        </form>
-      </S.PublishBox>
+              <input className="input-message" 
+                  type="text" 
+                  disabled={activeButtonPublish}
+                  placeholder="What's on your mind?" 
+                  value={publication.message} 
+                  onChange={(e)=>setPublication({...publication, message: e.target.value})}/>
+              <div className="containerButton">{/* TODO ARRUMAR GAMBIARRA PARA BOTÃO */}
+                <S.Button className="button-publish" type="submit">{loadingPublish}</S.Button>
+              </div>
+            </form>
+        </S.PostCardRightColumn>
+      </S.PublishCard>
       <Posts>
         {posts &&
           posts.map((post) => {
