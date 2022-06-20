ac757cf38875e5a2810654e0b28a8abf5408d5bf
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-20 08:34:35 -0300

    <feat> : search working when click in the search

--- src/components/SearchBar/SearchBarMobile.js
+++ src/components/SearchBar/SearchBarMobile.js
@@ -1,13 +1,29 @@
 import React from "react"
 import { IconContext } from "react-icons"
 import { DebounceInput } from "react-debounce-input"
+import axios from "axios"
 
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai"
 import * as S from "../../styles/style.js"
 
-
 export default function SearchBar() {
-  const [search, setSearch] = React.useState("")
+  const [username, setUsername] = React.useState("")
+
+  async function sendSearchUsername(e) {
+    e.preventDefault()
+    const URL = `${process.env.REACT_APP_API_URL}/user?username=${username}`
+
+    axios
+      .get(URL, username)
+      .then((res) => {
+        console.log(res.data)
+        console.log(URL)
+      })
+      .catch((err) => {
+        console.log(err)
+        console.log(URL)
+      })
+  }
 
   return (
     <S.SearchBarContainer>
@@ -16,11 +32,12 @@ export default function SearchBar() {
         debounceTimeout={300}
         placeholder="Search for people and friends"
         className="search-bar-input"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}/>
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <IconContext.Provider value={{ color: "#C6C6C6", size: "1.3rem" }}>
-        <AiOutlineSearch/>
+        <AiOutlineSearch onClick={sendSearchUsername} />
       </IconContext.Provider>
-      </S.SearchBarContainer>
+    </S.SearchBarContainer>
   )
 }
