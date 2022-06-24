6647c57dc26c19e3f24995bcde6942d45182133c
lemoscaio <caio_lemos94@hotmail.com>
2022-06-24 05:41:33 -0300

    feat: adds function to load more posts

--- src/pages/TimelinePage.js
+++ src/pages/TimelinePage.js
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import axios from "axios"
 import { LineWave } from "react-loader-spinner"
 import { useTheme } from "styled-components"
+import { IoReloadCircle } from "react-icons/io5"
+import useInterval from "react-useinterval"
 
 import PageLabel from "../components/shared/Labels/PageLabel.js"
 import PublishBox from "../components/shared/PublishBox/PublishBox.js"
@@ -9,7 +11,6 @@ import Posts from "../components/shared/Posts/Posts.js"
 import Post from "../components/shared/Posts/Post"
 import Trending from "../components/shared/Trending/Trending.js"
 import SearchBarMobile from "../components/SearchBar/SearchBarMobile.js"
-//import profilePic from "../assets/profile-placeholder.jpg"
 
 import * as S from "../styles/style.js"
 import { useAuth } from "../hooks/useAuth.js"
@@ -24,8 +25,17 @@ export default function TimelinePage() {
   const [followCount, setFollowCount] = useState(() => {
     getFollowCount()
   })
+  const [newPosts, setNewPosts] = useState()
+  const [lastPostCreateTime, setLastPostCreateTime] = useState(0)
+  useInterval(getNewPosts, 15000)
 
   const theme = useTheme()
+
+  function handleNewpostsClick() {
+    setPosts([...newPosts, ...posts])
+    setLastPostCreateTime(newPosts[0].createdAt)
+    setNewPosts([])
+  }
 
   function getFollowCount() {
     const config = {
@@ -69,6 +79,31 @@ export default function TimelinePage() {
       })
   }
 
+  function getNewPosts() {
+    const ORDERBY = "createdAt"
+    const ORDER_DIR = "desc"
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    }
+
+    if (posts && lastPostCreateTime) {
+      axios
+        .get(
+          `${process.env.REACT_APP_API_URL}/posts?order=${ORDERBY}&direction=${ORDER_DIR}&from_time=${lastPostCreateTime}`,
+          config,
+        )
+        .then((response) => {
+          setNewPosts([...response.data])
+        })
+        .catch((error) => {})
+    } else if (posts && !lastPostCreateTime) {
+      setLastPostCreateTime(posts[0].createdAt)
+    }
+  }
+
   function handleTryLoadAgain() {
     setLoadedPosts(false)
     setLoadPostsFail(false)
@@ -82,6 +117,16 @@ export default function TimelinePage() {
       <S.ContentWrapper>
         <S.MainContentWrapper>
           <PublishBox posts={posts} setPosts={setPosts} />
+          {newPosts?.length > 0 && (
+            <S.LoadPostsButtonContainer onClick={handleNewpostsClick}>
+              <S.Button2>
+                {newPosts.length} new posts, load more!{" "}
+                <span>
+                  <IoReloadCircle />
+                </span>
+              </S.Button2>
+            </S.LoadPostsButtonContainer>
+          )}
           <Posts>
             {loadedPosts &&
               posts &&
